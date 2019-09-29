b8432ee80bd0bc600f2f0fd4e989b9c5472cd2e6
Joe-P-W <52211404+Joe-P-W@users.noreply.github.com>
2019-09-29 22:08:34 +0100

    Added Snake class and init pygame
    Snake class added with segments arguement to determin the pos of each segment on screen and a direction arguement that will determin which direction the head will travel.

--- snake.py
+++ snake.py
@@ -1,1 +1,46 @@
 import pygame
+import sys
+import time
+
+pygame.init()
+
+resolution = (600, 600)
+squares = 100
+
+screen = pygame.display.set_mode(resolution)
+
+class Snake:
+    def __init__(self, segments, direction):
+        self.segments = segments
+        self.direction = direction
+        
+    def move_snake(self):
+        
+        if self.direction == "up":
+            new_head = (self.segments[0][0], self.segments[0][1]-1)
+            self.segments.insert(0, new_head)
+            self.segments.pop()
+                
+        
+snake = Snake([(50, 50), (50, 51), (50, 52)], "up")
+
+
+while True:
+    time.sleep(0.1)
+    for event in pygame.event.get():
+        
+        if event.type == pygame.QUIT: 
+            sys.exit()
+            
+    snake.move_snake()
+    
+    screen.fill((0, 0, 0))
+    
+    for segment in snake.segments:
+        
+        pos = ((segment[0]/squares)*resolution[0], (segment[1]/squares)*resolution[0],
+                resolution[0]/squares, resolution[1]/squares)
+                
+        pygame.draw.rect(screen, (255, 255, 255), pos)
+            
+    pygame.display.flip()
